fa6e07a786bdd3a95e31c477455750f14eee0863
Artem Herasymenko <86430417+ArtemHerasym@users.noreply.github.com>
2026-06-07 22:35:25 -0400

    Update query_11.sql

--- queries/query_11.sql
+++ queries/query_11.sql
@@ -6,5 +6,5 @@ JOIN students s ON g.student_id = s.id
 JOIN subjects sub ON g.subject_id = sub.id
 JOIN professors p ON sub.professor_id = p.id
 WHERE p.professor_name = 'Professor Johnny Benton'
-AND s.student_name = 'Jocelyn Zuniga';
+AND s.student_name = 'Jocelyn Zuniga'
 GROUP BY s.id, s.student_name, p.id, p.professor_name;
